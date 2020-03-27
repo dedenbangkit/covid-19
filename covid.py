dfe0c245b0f53c5011eeeab2338a0d3907ffdaff
dedenbangkit <mail@dedenbangkit.com>
2020-03-27 15:20:43 +0800

    First init

---
+++ covid.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import requests as r
+from datetime import datetime
+
+
+# In[2]:
+
+
+QUERY_URL = 'https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Cases_by_country_pt_V3/FeatureServer/0/query?f=json&where=(ADM0_NAME%20%3C%3E%20%27Serbia%27)%20AND%20(ADM0_NAME%20%3C%3E%20%27Kosovo%5B1%5D%27)&returnGeometry=true&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=78271&geometry=%7B%22xmin%22%3A-20037508.342788905%2C%22ymin%22%3A-20037508.342779063%2C%22xmax%22%3A20037508.342779063%2C%22ymax%22%3A20037508.342788905%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100&resultType=tile'
+
+
+# In[3]:
+
+
+data = r.get(QUERY_URL).json()
+features = data.get('features')
+
+
+# In[4]:
+
+
+for feature in features:
+    feature['attributes'].update({
+        'longitude' :feature['geometry']['x'],
+        'latitude' :feature['geometry']['y'],
+    })
+
+
+# In[5]:
+
+
+data = pd.DataFrame([feature['attributes'] for feature in features])
+data.to_csv("covid-19_{}.csv".format(datetime.now().strftime("%Y_%m_%d")))
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Covid Update
+
+Download coronavirus daily update as .csv
+
+## About Covid-19
+
+Coronavirus disease spreads primarily through contact with an infected person when they cough or sneeze. It also spreads when a person touches a surface or object that has the virus on it, then touches their eyes, nose, or mouth.
+Data Source: (Novel Coronavirus Situation Dashboard)[https://www.who.int/redirect-pages/page/novel-coronavirus-(covid-19)-situation-dashboard]
+
+## Usage
+
+```
+pip install -r requirements.txt
+python covid.py
+```
